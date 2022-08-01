828dc4a705aa412e16a07fdfd3f18010945d49cc
2swap <mchontz10@gmail.com>
2022-07-31 17:56:01 -0700

    Final commit. I am abandoning this project in favor of an Anki deck.

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -7,82 +7,81 @@ var y_values = [];
 var z_values = [];
 
 function drawThreeGeo(json, radius, shape) {
-    var json_geom = createGeometryArray(json);    
+    var json_geom = createGeometryArray(json);
     var convertCoordinates = getConversionFunctionName(shape);
-    
+
     for (var geom_num = 0; geom_num < json_geom.length; geom_num++) {
         let col = getRainbowColor(geom_num);
         if (new Date().getTime() / 500 % 2 == 1 && geom_num == question && guesses > 3) col = "white";
 
         if (json_geom[geom_num].type == 'Point') {
-            convertCoordinates(json_geom[geom_num].coordinates, radius);            
+            convertCoordinates(json_geom[geom_num].coordinates, radius);
             drawParticle(y_values[0], z_values[0], x_values[0]);
-            
+
         } else if (json_geom[geom_num].type == 'MultiPoint') {
             for (var point_num = 0; point_num < json_geom[geom_num].coordinates.length; point_num++) {
-                convertCoordinates(json_geom[geom_num].coordinates[point_num], radius);            
-                drawParticle(y_values[0], z_values[0], x_values[0]);                
+                convertCoordinates(json_geom[geom_num].coordinates[point_num], radius);
+                drawParticle(y_values[0], z_values[0], x_values[0]);
             }
-            
+
         } else if (json_geom[geom_num].type == 'LineString') {
-            
+
             for (var point_num = 0; point_num < json_geom[geom_num].coordinates.length; point_num++) {
                 convertCoordinates(json_geom[geom_num].coordinates[point_num], radius);
-            }            
+            }
             drawLine(y_values, z_values, x_values, col);
-            
+
         } else if (json_geom[geom_num].type == 'Polygon') {
             for (var segment_num = 0; segment_num < json_geom[geom_num].coordinates.length; segment_num++) {
                 for (var point_num = 0; point_num < json_geom[geom_num].coordinates[segment_num].length; point_num++) {
-                    convertCoordinates(json_geom[geom_num].coordinates[segment_num][point_num], radius); 
-                }                
+                    convertCoordinates(json_geom[geom_num].coordinates[segment_num][point_num], radius);
+                }
                 drawLine(y_values, z_values, x_values, col);
             }
-            
+
         } else if (json_geom[geom_num].type == 'MultiLineString') {
             for (var segment_num = 0; segment_num < json_geom[geom_num].coordinates.length; segment_num++) {
                 for (var point_num = 0; point_num < json_geom[geom_num].coordinates[segment_num].length; point_num++) {
-                    convertCoordinates(json_geom[geom_num].coordinates[segment_num][point_num], radius); 
-                }                
-                drawLine(y_values, z_values, x_values, col);                
+                    convertCoordinates(json_geom[geom_num].coordinates[segment_num][point_num], radius);
+                }
+                drawLine(y_values, z_values, x_values, col);
             }
-            
+
         } else if (json_geom[geom_num].type == 'MultiPolygon') {
             for (var polygon_num = 0; polygon_num < json_geom[geom_num].coordinates.length; polygon_num++) {
                 for (var segment_num = 0; segment_num < json_geom[geom_num].coordinates[polygon_num].length; segment_num++) {
                     for (var point_num = 0; point_num < json_geom[geom_num].coordinates[polygon_num][segment_num].length; point_num++) {
-                        convertCoordinates(json_geom[geom_num].coordinates[polygon_num][segment_num][point_num], radius); 
-                    }                    
-                    drawLine(y_values, z_values, x_values, col);                    
+                        convertCoordinates(json_geom[geom_num].coordinates[polygon_num][segment_num][point_num], radius);
+                    }
+                    drawLine(y_values, z_values, x_values, col);
                 }
             }
         } else {
             throw new Error('The geoJSON is not valid.');
-        }        
+        }
     }
-}       
+}
 function createGeometryArray(json) {
     var geometry_array = [];
 
     if (json.type == 'Feature') {
-        geometry_array.push(json.geometry);        
+        geometry_array.push(json.geometry);
     } else if (json.type == 'FeatureCollection') {
-        for (var feature_num = 0; feature_num < json.features.length; feature_num++) { 
-            geometry_array.push(json.features[feature_num].geometry);            
+        for (var feature_num = 0; feature_num < json.features.length; feature_num++) {
+            geometry_array.push(json.features[feature_num].geometry);
         }
     } else if (json.type == 'GeometryCollection') {
-        for (var geom_num = 0; geom_num < json.geometries.length; geom_num++) { 
+        for (var geom_num = 0; geom_num < json.geometries.length; geom_num++) {
             geometry_array.push(json.geometries[geom_num]);
         }
     } else {
         throw new Error('The geoJSON is not valid.');
-    }    
-    //alert(geometry_array.length);
+    }
     return geometry_array;
 }
 function getConversionFunctionName(shape) {
     var conversionFunctionName;
-    
+
     if (shape == 'sphere') {
         conversionFunctionName = convertToSphereCoords;
     } else if (shape == 'plane') {
@@ -100,13 +99,13 @@ function convertToSphereCoords(coordinates_array, sphere_radius) {
 
     x_values.push(Math.cos(lat * Math.PI/180) * Math.cos(lon * Math.PI/180) * sphere_radius);
     y_values.push(Math.cos(lat * Math.PI/180) * Math.sin(lon * Math.PI/180) * sphere_radius);
-    z_values.push(Math.sin(lat * Math.PI/180) * sphere_radius);    
+    z_values.push(Math.sin(lat * Math.PI/180) * sphere_radius);
 }
 function convertToPlaneCoords(coordinates_array, radius) {
     var lon = coordinates_array[0];
     var lat = coordinates_array[1];
     var plane_offset = radius / 2;
-        
+
     z_values.push((lat/180) * radius);
     y_values.push((lon/180) * radius);
 }
@@ -114,22 +113,22 @@ function convertToPlaneCoords(coordinates_array, radius) {
 function drawParticle(x, y, z) {
     var particle_geom = new THREE.Geometry();
     particle_geom.vertices.push(new THREE.Vector3(x, y, z));
-    
+
     var particle_material = new THREE.PointsMaterial({color:"red", size:.008});
-    
+
     var particle = new THREE.Points(particle_geom, particle_material);
     scene.add(particle);
-    
+
     clearArrays();
 }
 function drawLine(x_values, y_values, z_values, col) {
     var line_geom = new THREE.Geometry();
     createVertexForEachPoint(line_geom, x_values, y_values, z_values);
-                
+
     var line_material = new THREE.LineBasicMaterial({color:"red", linewidth:1});
     var line = new THREE.Line(line_geom, line_material);
     scene.add(line);
-    
+
     clearArrays();
 }
 
@@ -143,7 +142,7 @@ function createVertexForEachPoint(object_geometry, values_axis1, values_axis2, v
 function clearArrays() {
     x_values.length = 0;
     y_values.length = 0;
-    z_values.length = 0;    
+    z_values.length = 0;
 }
 
 const getRainbowColor = (t) => {
@@ -238,51 +237,52 @@ for(var x = 0; x < globeImages.length; x++){
 
 //Region files
 var filenames = [
-"My Cities",
+//"My Cities",
 "Malaysia",
 "Cities",
 "Elevation Points",
-"More Populated",
+//"More Populated",
 "Points of Interest",
 "Populated Places Simple",
 
-"Bodies of Water",
-"Brazil",
-"Canada",
-"Canadian Islands",
-"Chile",
-"China",
-"Continents",
-"Countries",
-"Deserts",
-"Disputed Areas",
-"Egypt",
-"Germany",
-"Hawaii",
-"Highlands",
-"India",
-"Indonesian Islands",
-"Islands",
-"Japan",
-"Mexico",
-"Mountain Ranges",
-"Nigeria",
-"North American Lakes",
-"Oceans",
-"Overseas Territories",
-"Parks and Protected Lands",
-"Pakistan",
-"Peninsulas",
-"Philippine Islands",
-"Philippines",
-"Regions",
-"Rivers",
+//"Bodies of Water",
+//"Brazil",
+//"Canada",
+//"Canadian Islands",
+//"Chile",
+//"China",
+//"Continents",
+//"Countries",
+//"Deserts",
+//"Disputed Areas",
+//"Egypt",
+//"Germany",
+//"Hawaii",
+//"Highlands",
+//"India",
+//"Indonesian Islands",
+//"Islands",
+//"Japan",
+//"Mexico",
+//"Mountain Ranges",
+//"Nigeria",
+//"North American Lakes",
+//"Oceans",
+//"Overseas Territories",
+//"Parks and Protected Lands",
+//"Pakistan",
+//"Peninsulas",
+//"Philippine Islands",
+//"Philippines",
+//"Regions",
+//"Rivers",
+"anki",
 //"Russia",
 "South Africa",
 "Spain",
 "UK Counties",
 "UK Regions",
-"USA Counties",
+//"USA Counties",
 "USA States",
 ];
 
